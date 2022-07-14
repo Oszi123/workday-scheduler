e0ad77b9e4cae6c38a531eec591721906422fa8b
osb1996 <oscar.bonilla123@outlook.com>
2022-07-14 12:00:48 -0400

    variables for rows and columns made

--- script.js
+++ script.js
@@ -8,4 +8,16 @@ function getLocalStorage(key) {
 $(document).ready(function () {
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
     $("#currentDay").css("text-decoration", "underline");
-    for (let i = 9; i < 18; i++) 
+    for (let i = 9; i < 18; i++) {
+
+
+        var row = $(`<div data-time=${i} id='${i}' class="row">`);
+
+
+        var col1 = $('<div class="col-sm-2"> <p class="hour">' + amPm(i) + '</p>');
+
+
+        var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="event" placeholder="Add your event here..."></textarea>`);
+
+
+        var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
